34597a69a5f7365475627c409a2a5aaaaca4769c
yiouejv <yiouejv@126.com>
2021-09-22 20:51:34 +0800

    原型模式

---
+++ 原型模式/copy_ctor.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Resume {
+public:
+	Resume() {
+
+	}
+
+	void SetName() {
+		
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
