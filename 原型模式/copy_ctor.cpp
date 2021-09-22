47fad369eac20e0c03289e714570fb6e1929c2e5
yiouejv <yiouejv@12.com>
2021-09-22 23:48:27 +0800

    原型模式

--- 原型模式/copy_ctor.cpp
+++ 原型模式/copy_ctor.cpp
@@ -1,19 +1,40 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 class Resume {
+private:
+    string m_name;
+    int m_age;
 public:
-	Resume() {
+    Resume() { }
+    Resume(const Resume& resume) {
+        m_name = resume.m_name;
+        m_age = resume.m_age;
+    }
 
-	}
+    void SetInfo(const string& name, int age) {
+        m_name = name;
+        m_age = age;
+    }
 
-	void SetName() {
-		
-	}
+    void display() {
+        cout << "名字是: " << m_name << endl
+             << "年龄是: " << m_age << endl;
+    }
 };
 
 int main(int argc, char const *argv[])
 {
-	
-	return 0;
+    Resume resume;
+    resume.SetInfo("杨小哥", 18);
+    resume.display();
+
+    Resume resume2 = resume;
+    resume2.display();
+
+    Resume *p = &resume;
+    Resume resume3 = *p;
+    resume3.display();
+    return 0;
 }
